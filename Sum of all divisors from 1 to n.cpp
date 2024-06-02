589f51e0b99a02adec26b4edd3e350edadc98c74
Md. Sadman Hasan Talukder <133516078+sadmanHT@users.noreply.github.com>
2024-06-03 01:26:25 +0600

    Update Sum of all divisors from 1 to n.cpp

--- Sum of all divisors from 1 to n.cpp
+++ Sum of all divisors from 1 to n.cpp
@@ -1,40 +1,40 @@
 //{ Driver Code Starts
 #include<bits/stdc++.h> 
-using namespace std;
+using namespace std; 
 
 // } Driver Code Ends
-class Solution{
+//User function Template for C++
+class Solution
+{
 public:
-    int evenlyDivides(int N){
-        //The digits of the number
-        int count= 0;
-        int digit;
-        int actual_num = N;
-        while(N>0){
-            digit = N%10;
-        
-        if(digit!= 0 && actual_num%digit == 0){
-            count++;
+    long long sumOfDivisors(int N)
+    {
+        long long ans = 0;
+        for(int i = 1; i<=N; i++){
+            if(N%i==0){
+                ans += N;
+            }
+            else{
+                ans+= i*(N/i);
+            }
         }
-        N = int(N/10);
-        }
-        
-        return count;
+        return ans;
     }
 };
 
 //{ Driver Code Starts.
-int main() 
-{ 
+int main()
+{
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         int N;
         cin>>N;
         Solution ob;
-        cout << ob.evenlyDivides(N) << endl;
+        long long ans  = ob.sumOfDivisors(N);
+        cout<<ans<<endl;
     }
-    return 0; 
+    return 0;
 }
 // } Driver Code Ends
